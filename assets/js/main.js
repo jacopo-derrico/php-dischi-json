6fc633de35649968d86ed0d65e0dee128165565d
jacopo-derrico <derricoj.design@gmail.com>
2024-02-28 15:11:16 +0100

    api call from server

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      title: 'Hello Vue!'
+      title: 'Hello Vue!',
+      apiUrl: 'server.php',
+      diskList: [],
     }
-  }
+  },
+  mounted() {
+    this.callApi()
+  },
+  methods: {
+    callApi() {
+      axios.get(this.apiUrl)
+        .then((response) => {
+          this.diskList = response.data;
+        })
+    }
+  },
 }).mount('#app')
